49200c952f78015395e888081853609d57c76611
ElnuDev <elnu@elnu.com>
2022-02-28 20:01:32 -0800

    Use explicit homebrew Hugo install path in build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hugo && find public -path "*.html" -type f -exec tidy --quiet yes --drop-empty-elements no --hide-comments yes -o {} {} \;
+/home/linuxbrew/.linuxbrew/bin/hugo && find public -path "*.html" -type f -exec tidy --quiet yes --drop-empty-elements no --hide-comments yes -o {} {} \;
